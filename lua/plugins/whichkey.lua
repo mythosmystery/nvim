7e225c652f682bd76ed1730dac1a1cc1f42e22af
Hunter Barton <axiysfire@gmail.com>
2024-01-15 01:20:30 -0600

    .

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -48,8 +48,6 @@ return {
 			[" "] = { "<cmd> Telescope find_files<cr>", "Search" },
 			t = {
 				name = "Terminal",
-				n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-				p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 				f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 				h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 				v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
