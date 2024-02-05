ebce45723f6d8697ef879314ee0edec136c587aa
Hunter Barton <axiysfire@gmail.com>
2024-02-05 16:57:57 -0600

    Update bufferline.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,6 +5,11 @@ return {
   config = function()
     require("bufferline").setup({
       options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
         offsets = {
           {
             filetype = "undotree",
