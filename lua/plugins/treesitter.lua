6c9b3474d7fc3f666cc529a8fe57bb2f4c1a5ebf
Hunter Barton <axiysfire@gmail.com>
2024-02-05 23:00:36 -0600

    add treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-      auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-    vim.treesitter.language.register('markdown', 'octo')
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
+			})
+			vim.treesitter.language.register("markdown", "octo")
+		end,
+	},
 }
