5e40b1025f742fca3805746e7610e46aa786eb70
Hunter Barton <axiysfire@gmail.com>
2024-02-06 12:41:41 -0600

    update

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,3 +1,17 @@
+local function move_line_up()
+  print("move_line_up")
+  if vim.fn.line(".") > 1 then
+    vim.cmd('m -2')
+  end
+end
+
+local function move_line_down()
+  print("move_line_down")
+  if vim.fn.line(".") < vim.fn.line("$") then
+    vim.cmd('m +1')
+  end
+end
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.loaded_netrw = 1
@@ -15,10 +29,10 @@ vim.cmd("set scrolloff=8")
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<C-c>', '<ESC>', opts)
-vim.keymap.set('n', '<C-H>', '<C-W>h', opts)
-vim.keymap.set('n', '<C-J>', '<C-W>j', opts)
-vim.keymap.set('n', '<C-K>', '<C-W>k', opts)
-vim.keymap.set('n', '<C-L>', '<C-W>l', opts)
 vim.keymap.set('n', '<C-i>', ':noh<CR>', opts)
+vim.keymap.set('v', '<PageDown>', ':m \'>+1<CR>gv=gv', opts)
+vim.keymap.set('v', '<PageUp>', ':m \'<-2<CR>gv=gv', opts)
+vim.keymap.set('n', '<PageDown>', move_line_down, opts)
+vim.keymap.set('n', '<PageUp>', move_line_up, opts)
 
 vim.filetype.add({ extension = { templ = "templ" } })
