d7ca1f90d829cb149433e592b396611dee043c5e
Hunter <axiysfire@gmail.com>
2024-08-02 21:23:43 -0500

    fix whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -7,21 +7,17 @@ return {
 	end,
 	config = function()
 		local wk = require("which-key")
-		wk.register({
-			d = { name = "[D]ap", _ = "which_key_ignore" },
-			y = { name = "[Y]chat", _ = "which_key_ignore" },
-			c = { name = "[C]ode", _ = "which_key_ignore" },
-			t = { name = "[T]erminal", _ = "which_key_ignore" },
-			u = { name = "[U]ndo", _ = "which_key_ignore" },
-			g = {
-				name = "[G]it",
-				_ = "which_key_ignore",
-				o = { name = "[O]cto (Github)", _ = "which_key_ignore" },
-			},
-			n = { name = "[N]otes", _ = "which_key_ignore" },
-			f = { name = "[F]ind", _ = "which_key_ignore" },
-			x = { name = "[X]rouble", _ = "which_key_ignore" },
-			P = { name = "[P]lugins", _ = "which_key_ignore" },
-		}, { prefix = "<leader>" })
+		wk.add({
+			{ "<leader>y", { group = "[Y]chat" } },
+			{ "<leader>d", { group = "[D]ap" } },
+			{ "<leader>c", { group = "[C]ode" } },
+			{ "<leader>t", { group = "[T]erminal" } },
+			{ "<leader>u", { group = "[U]ndo" } },
+			{ "<leader>g", { group = "[G]it" } },
+			{ "<leader>n", { group = "[N]otes" } },
+			{ "<leader>f", { group = "[F]ind" } },
+			{ "<leader>x", { group = "[X]rouble" } },
+			{ "<leader>P", { group = "[P]lugins" } },
+		})
 	end,
 }
