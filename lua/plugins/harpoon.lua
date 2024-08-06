5ca07436a378e82e2af15929ae31c61322105e1d
Hunter <axiysfire@gmail.com>
2024-08-06 14:35:42 -0500

    update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,9 +10,9 @@ return {
 				},
 			})
 			vim.keymap.set("n", "<leader>h", require("harpoon.ui").toggle_quick_menu, { desc = "[H]arpoon" })
-			vim.keymap.set("n", "<leader>hm", require("harpoon.mark").add_file, { desc = "Add [M]ark" })
-			vim.keymap.set("n", "<leader>hn", require("harpoon.ui").nav_next, { desc = "[N]ext" })
-			vim.keymap.set("n", "<leader>hp", require("harpoon.ui").nav_prev, { desc = "[P]rev" })
+			vim.keymap.set("n", "<leader>m", require("harpoon.mark").add_file, { desc = "Add [M]ark" })
+			vim.keymap.set("n", "<leader>n", require("harpoon.ui").nav_next, { desc = "[N]ext" })
+			vim.keymap.set("n", "<leader>p", require("harpoon.ui").nav_prev, { desc = "[P]rev" })
 		end,
 	},
 	{
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -8,13 +8,11 @@ return {
 	config = function()
 		local wk = require("which-key")
 		wk.add({
-			{ "<leader>y", { group = "[Y]chat" } },
 			{ "<leader>d", { group = "[D]ap" } },
 			{ "<leader>c", { group = "[C]ode" } },
 			{ "<leader>t", { group = "[T]erminal" } },
 			{ "<leader>u", { group = "[U]ndo" } },
 			{ "<leader>g", { group = "[G]it" } },
-			{ "<leader>n", { group = "[N]otes" } },
 			{ "<leader>f", { group = "[F]ind" } },
 			{ "<leader>x", { group = "[X]rouble" } },
 			{ "<leader>P", { group = "[P]lugins" } },
